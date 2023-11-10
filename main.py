f31649154340d410f72f5a5ec93e2d84d014a34f
sethclim <sethclim@gmail.com>
2023-11-10 01:50:13 -0500

    working kafka consumer + logging

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 
 from kafka import KafkaConsumer
 
+import logging
+logging.basicConfig(level=logging.INFO)
+
 def main():
-    consumer = KafkaConsumer(bootstrap_servers='localhost:1234')
-    consumer.subscribe(['my_favorite_topic'])
-    for msg in consumer:
-        print (msg)
+    print("Running AI")
+
+    logger = logging.getLogger(__name__)
+    try:
+
+        consumer = KafkaConsumer('my-topic',
+                                group_id='my-group',
+                                bootstrap_servers=['kafka:9092'])
+
+        for message in consumer:
+            logger.info("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                                message.offset, message.key,
+                                                message.value))
+    except Exception as ex:
+        logger.error(ex)
+
 
 if __name__ == "__main__":
     main()
