cf666a16c773a822ee2ef9acb54bb0b70b062d55
sethclim <sethclim@gmail.com>
2023-11-10 03:20:18 -0500

    adding generate recipe method

--- main.py
+++ main.py
@@ -2,12 +2,29 @@
 import logging
 logging.basicConfig(level=logging.INFO)
 
+import instructor
+from openai import OpenAI
+from entities import Recipe
+
+client = instructor.patch(OpenAI())
+
+def generate_recipe(input) -> Recipe:
+    return client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": f"Help me understand the following by describing it as a detailed knowledge graph: {input}",
+            }
+        ],
+        response_model=Recipe,
+    )
+
 def main():
-    print("Running AI...")
+    logger = logging.getLogger()
+    logger.info("Running AI...")
 
-    logger = logging.getLogger(__name__)
-
-
+    generate_recipe("chicken")
 
 if __name__ == "__main__":
     main()
