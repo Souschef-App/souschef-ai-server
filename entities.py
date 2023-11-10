4ead7afabc80b7a6178224d070ef365d4d199977
sethclim <sethclim@gmail.com>
2023-11-10 03:14:40 -0500

    defined entities for chat-gpt

---
+++ entities.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, Field
+from enum     import Enum, auto
+from typing   import List
+
+class Unit(Enum):
+    NONE        = auto()
+    Ounces      = auto()  #Start of weight units
+    Pounds      = auto()
+    Grams       = auto()
+    Kilograms   = auto()  #End of weight units
+    Teaspoons   = auto()  #Start of volume units
+    Tablespoons = auto()
+    Cups        = auto()
+    Pints       = auto()
+    Quarts      = auto()
+    Gallons     = auto()
+    Liters      = auto()  #End of volume units
+
+class Ingredient(BaseModel):
+    name : str
+    quantity : float
+    unit : Unit = Field(description="Correctly assign one of the predefined units to the ingredient.")
+
+class Kitchenware(BaseModel):
+    name : str
+    quantity: int
+
+class Task(BaseModel):
+    title: str
+    description: str
+    ingredients: List[Ingredient] = Field(..., default_factory=list)
+    kitchenware: List[Kitchenware] = Field(..., default_factory=list)
+    difficulty: int 
+    dependencies: List[str] = Field(..., default_factory=list)
+
+class Recipe(BaseModel):
+    tasks: List[Task] = Field(..., default_factory=list)
+    
