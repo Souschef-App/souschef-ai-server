fb0b21a27faa4b13968b9b20bd949fc22a04ea59
sethclim <sethclim@gmail.com>
2023-11-10 03:18:00 -0500

    removed kafka

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 
-from kafka import KafkaConsumer
-
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -8,18 +6,7 @@ def main():
     print("Running AI...")
 
     logger = logging.getLogger(__name__)
-    try:
 
-        consumer = KafkaConsumer('my-topic',
-                                group_id='my-group',
-                                bootstrap_servers=['kafka:9092'])
-
-        for message in consumer:
-            logger.info("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                                message.offset, message.key,
-                                                message.value))
-    except Exception as ex:
-        logger.error(ex)
 
 
 if __name__ == "__main__":
